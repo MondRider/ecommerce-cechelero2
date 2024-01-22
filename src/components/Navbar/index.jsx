26c6834c5523b7c94090a1e4711730f5f1bf44e7
MondRider <Gabriel.aline.ga62@gmail.com>
2024-01-21 22:05:00 -0300

    Navbar
    Modelo de navbar feito.

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,19 @@
+import './style.css'
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <label className='logo'>
+        Placeholder
+      </label>
+      <ul>
+        <li className='active'>Home</li>
+        <li>Sobre</li>
+        <li>Serviços</li>
+        <li>Contato</li>
+        <li>Feedback</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
