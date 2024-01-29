4f8116f1109dcf3c9394031c1a76d6147dc6ee6d
MondRider <Gabriel.aline.ga62@gmail.com>
2024-01-29 19:42:39 -0300

    Stock Count

--- src/App.js
+++ src/App.js
@@ -2,13 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [stock, setStock] = useState(0);
+
   return (
     <div className="App">
      <Navbar/>
      <ItemListContainer greeting={'Bem vindo'} />
+     <ItemCount stock={15} count={count} setCount={setCount} />
     </div>
+  );
+}
+
+function ItemCount({ stock, count, setCount }) {
+  function handleAdd() {
+    if (count < stock) setCount(count + 1);
+  }
+
+  function handleRemove() {
+    if (count > 0) setCount(count - 1);
+  }
+
+  return (
+    <main>
+      <button onClick={handleRemove}>-</button>
+      <p>{count}</p>
+      <button onClick={handleAdd}>+</button>
+    </main>
   );
 }
 
